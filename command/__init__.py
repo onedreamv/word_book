256ea211954f359d06a55de9a6054e5780353608
dream <mikuandlivi@gmail.com>
2026-03-27 11:10:57 +0800

    feat: 添加 clean 命令用于从文本中提取英文单词

--- command/__init__.py
+++ command/__init__.py
@@ -2,10 +2,12 @@ import typer
 
 from .book_init import app as init_app
 from .add import app as add_app
+from .clean import app as clean_app
 from .merge import app as merge_app
 
 app = typer.Typer()
 
 app.add_typer(init_app, name="init")
 app.add_typer(add_app, name="add")
+app.add_typer(clean_app, name="clean")
 app.add_typer(merge_app, name="merge")
